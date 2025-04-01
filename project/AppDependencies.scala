377f929f699eb6f926909925e8fa9a48116a1abc
Ian Walton <51008396+ian-1@users.noreply.github.com>
2025-04-01 14:25:14 +0100

    APB-9326 - update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,19 +8,19 @@ object AppDependencies {
 
   lazy val compileDeps = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "com.github.blemale" %% "scaffeine" % "5.2.1",
-    "uk.gov.hmrc" %% "agent-mtd-identifiers" % "2.1.0",
-    "uk.gov.hmrc" %% "play-hal-play-30" % "4.0.0",
-    "uk.gov.hmrc" %% "play-hmrc-api-play-30" % "8.0.0"
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-30" % bootstrapVersion,
+    "com.github.blemale"      %% "scaffeine"                  % "5.3.0",
+    "uk.gov.hmrc"             %% "agent-mtd-identifiers"      % "2.1.0",
+    "uk.gov.hmrc"             %% "play-hal-play-30"           % "4.0.0",
+    "uk.gov.hmrc"             %% "play-hmrc-api-play-30"      % "8.0.0"
   )
 
   val test  = Seq(
-    "org.scalatestplus.play" %% "scalatestplus-play" % "7.0.1" % Test,
-    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % Test,
-    "org.pegdown" % "pegdown" % "1.6.0" % Test,
-    "org.scalamock" %% "scalamock" % "6.0.0" % Test,
-    "com.vladsch.flexmark" %  "flexmark-all" % "0.64.8" % Test
+    "org.scalatestplus.play"  %% "scalatestplus-play"         % "7.0.1"           % Test,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion  % Test,
+    "org.pegdown"             % "pegdown"                     % "1.6.0"           % Test,
+    "org.scalamock"           %% "scalamock"                  % "7.3.0"           % Test,
+    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.64.8"          % Test
   )
 
 }
