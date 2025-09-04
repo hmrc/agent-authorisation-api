b2cd0385d753334adc4cbe717ff6da22913965f4
wojciech.gradzki <gitwojciech@users.noreply.github.com>
2025-09-04 12:24:06 +0100

    [WG][APB-9831] dependencies update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,11 +3,12 @@ import sbt.*
 
 object AppDependencies {
   private val mongoVer = "2.7.0"
-  private val bootstrapVersion: String = "9.16.0"
+  private val bootstrapVersion: String = "10.1.0"
 
   lazy val compileDeps: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc"        %% "bootstrap-backend-play-30"  % bootstrapVersion,
     "com.github.blemale" %% "scaffeine"                  % "5.3.0",
     "uk.gov.hmrc"        %% "play-hal-play-30"           % "4.0.0",
     "uk.gov.hmrc"        %% "play-hmrc-api-play-30"      % "8.0.0",
