8474f613c4648ca882d7c02bd3920f21f8480351
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2025-11-03 12:43:52 +0000

    APB-9523: Updates dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,24 +3,23 @@ import sbt.*
 
 object AppDependencies {
   private val mongoVer = "2.10.0"
-  private val bootstrapVersion: String = "10.2.0"
+  private val bootstrapVersion: String = "10.3.0"
 
   lazy val compileDeps: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc"        %% "bootstrap-backend-play-30"  % bootstrapVersion,
-    "com.github.blemale" %% "scaffeine"                  % "5.3.0",
-    "uk.gov.hmrc"        %% "play-hal-play-30"           % "4.0.0",
-    "uk.gov.hmrc"        %% "play-hmrc-api-play-30"      % "8.0.0",
     "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-30"         % mongoVer,
+    "uk.gov.hmrc"        %% "play-hmrc-api-play-30"      % "8.0.0",
+    "uk.gov.hmrc"        %% "play-hal-play-30"           % "4.0.0",
     "uk.gov.hmrc"        %% "domain-play-30"             % "11.0.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion % Test,
-    "org.scalamock"     %% "scalamock"               % "7.4.1"          % Test,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % mongoVer         % Test,
-    "org.scalacheck"    %% "scalacheck"              % "1.18.1"         % Test
-  )
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % mongoVer,
+    "org.scalamock"     %% "scalamock"               % "7.5.1",
+    "org.scalacheck"    %% "scalacheck"              % "1.19.0"
+  ).map(_ % Test)
 
 }
