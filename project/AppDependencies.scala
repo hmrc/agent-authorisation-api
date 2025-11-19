96cf7b28e4563b12cd135ebd92df561e70bbb8e6
Geoff Watson <9043337+geoffreywatson@users.noreply.github.com>
2025-11-19 14:14:17 +0000

    [APB-10034][GW] fix documentation postcode regex (#150)
    * [APB-10034][GW] fix documentation postcode regex
    
    * update dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 
 object AppDependencies {
   private val mongoVer = "2.10.0"
-  private val bootstrapVersion: String = "10.3.0"
+  private val bootstrapVersion: String = "10.4.0"
 
   lazy val compileDeps: Seq[ModuleID] = Seq(
     ws,
