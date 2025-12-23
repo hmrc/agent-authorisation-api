4ccdd609eefcf72bfb44b3d16fa4e21250007de8
Iyad Issa <237692191+iscalit@users.noreply.github.com>
2025-12-23 13:52:40 +0000

    APB-10433: New de-auth endpoint

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import play.sbt.PlayImport.ws
 import sbt.*
 
 object AppDependencies {
-  private val mongoVer = "2.10.0"
+  private val mongoVer = "2.11.0"
   private val bootstrapVersion: String = "10.4.0"
 
   lazy val compileDeps: Seq[ModuleID] = Seq(
@@ -10,8 +10,8 @@ object AppDependencies {
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc"        %% "bootstrap-backend-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-30"         % mongoVer,
-    "uk.gov.hmrc"        %% "play-hmrc-api-play-30"      % "8.0.0",
-    "uk.gov.hmrc"        %% "play-hal-play-30"           % "4.0.0",
+    "uk.gov.hmrc"        %% "play-hmrc-api-play-30"      % "8.3.0",
+    "uk.gov.hmrc"        %% "play-hal-play-30"           % "4.1.0",
     "uk.gov.hmrc"        %% "domain-play-30"             % "11.0.0"
   )
 
